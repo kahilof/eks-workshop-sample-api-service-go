73e29169a834b55a862bb57084ae2eef3f891fa8
Omar <omarkahil@outlook.com>
2021-02-15 16:51:06 +0000

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World EKS"}
+		res := &response{Message: "Hello World Omar"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
